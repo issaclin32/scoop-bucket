dc23600dd6b80e92e0fae773513dad15699fea89
Issac Lin <jukumusahyu@gmail.com>
2023-10-03 23:58:48 +0800

    scoop-check: Update gsudo first

--- scripts/scoop-check.ps1
+++ scripts/scoop-check.ps1
@@ -2,6 +2,7 @@ scoop update
 scoop status
 $choice = Read-Host "Update All [Y/N] "
 if ($choice.ToLower() -eq 'y') {
+    scoop update gsudo
     sudo scoop update *
     scoop cleanup *
     scoop cache rm *
